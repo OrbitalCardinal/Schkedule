09e9ad43a7d789c0a29a3b21c33a951549326015
OrbitalCardinal <raulcepedac1@gmail.com>
2022-04-04 18:39:51 -0500

    Pagina de proyecto (Solo interfaces)

---
+++ src/app/components/activity-card/activity-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'activity-card',
+    templateUrl: './activity-card.component.html',
+    styleUrls: ['./activity-card.component.scss']
+})
+
+export class ActivityCardComponent {}
---
+++ src/app/pages/recent-projects-page/recent-projects-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'recent-projects-page',
+    templateUrl: './recent-projects-page.component.html',
+    styleUrls: ['./recent-projects-page.component.scss']
+})
+
+export class RecentProjectsPageComponent {
+    test() {
+        console.log("TEST")
+    }
+}
